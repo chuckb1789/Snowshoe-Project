f3b00c6ead186984045b2fc712dc32a00e8173e1
CharlieBeck <chuckb1789@gmail.com>
2017-01-20 23:10:37 -0700

    Version 1.0.0

---
+++ rmnp.js
@@ -0,0 +1,48 @@
+angular.module('snowApp', [])
+       .controller ('snowController', snowFunction);
+
+snowFunction.$inject = ['$http']
+
+function snowFunction($http) {
+  var snowCtrl = this;
+
+  snowCtrl.title = 'Project Snowshoe'
+
+    snowCtrl.currently=[];
+    snowCtrl.weather=[];
+
+    snowCtrl.getWeather = function () {
+    var APIKEY =process.env.APIKEY
+    var url ='https://api.darksky.net/forecast/'+APIKEY+'/40.3292,-105.5933'
+    console.log("looking for", snowCtrl.currently, "at", url);
+    $http.get(url)
+        .then(function success(response) {
+            console.log(response.data)
+            snowCtrl.currently = response.data.currently
+            snowCtrl.weather = response.data.hourly
+            snowCtrl.daily = response.data.daily.data[0]
+            snowCtrl.avalanche = response.data.alerts[0]
+
+        },
+        function failure(response) {
+            console.log("ERROR", response)
+        })
+      };
+
+     snowCtrl.getSnow = function () {
+        var ID = process.env.ID
+        var APIKEY2 = process.env.APIKEY2
+        var url = 'https://api.weatherunlocked.com/api/snowreport/303011?app_id='+ID+'&app_key='+APIKEY2
+        console.log("making http request")
+        $http.get(url)
+            .then(function (response) {
+                console.log(response.data)
+                snowCtrl.snow = response.data
+                console.log(snowCtrl.snow)
+
+            },
+            function failure(response) {
+                console.log("ERROR", response)
+            })
+          }
+}
