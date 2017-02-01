4fbdbddd7b31c486a5058e8d6d5a755aafce75d9
CharlieBeck <chuckb1789@gmail.com>
2017-02-01 15:04:44 -0700

    update server

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(logger('dev'))
 //=================ROUTE VARIABLES==========================
 crestedURL = 'https://api.darksky.net/forecast/'+APIKEY+'/38.911024,-107.031255';
 
-
 friscoURL = 'https://api.darksky.net/forecast/'+APIKEY+'/39.7292,-105.9047';
 
 indianPeaksURL = 'https://api.darksky.net/forecast/'+APIKEY+'/39.9614, -105.5108';
@@ -33,53 +32,52 @@ wpURL = 'https://api.darksky.net/forecast/'+APIKEY+'/39.8472,-105.9117';
 
 app.get('/forecast/38.911024,-107.031255', function(req,res) {
     request.get(crestedURL,function(error,response,body){
-    return body })})
+    res.send(body) })})
 
 
 //==================ROUTE FOR FRISCO=========================
 
  app.get('/forecast/39.7292,-105.9047', function(req,res) {
      request.get(friscoURL,function(error,response,body){
-     return body }) })
+     res.send(body) }) })
 
 
 //==================ROUTE FOR INDIAN PEAKS===================
 app.get('/forecast/39.9614, -105.5108', function(req,res) {
     request.get(indianPeaksURL,function(error,response,body){
-    return body }) })
+    res.send(body) }) })
 
 
 //==================ROUTE FOR KENOSHA PASS===================
 
 app.get('/forecast/39.4133,-105.7567', function(req,res) {
     request.get(kenoshaURL,function(error,response,body){
-    return body }) })
+    res.send(body) }) })
 
 //==================ROUTE FOR MOSQUITO PASS==================
 
 app.get('/forecast/39.2814,-106.1861', function(req,res) {
     request.get(mosquitoURL,function(error,response,body){
-    return body }) })
+    res.send(body) }) })
 
 
 //==================ROUTE FOR ROCKY MOUNTAIN=================
 
 app.get('/forecast/40.3292,-105.5933', function(req,res) {
     request.get(rmnpURL,function(error,response,body){
-    return body }) })
+    res.send(body) }) })
 
 
 //==================ROUTE FOR STEAMBOAT SPRINGS==============
 app.get('/forecast//40.3847,-106.6117', function(req,res) {
     request.get(steamboatURL,function(error,response,body){
-    return body }) })
+    res.send(body) }) })
 
 
 //==================ROUTE FOR WINTER PARK====================
 app.get('/forecast//39.8472,-105.9117', function(req,res) {
     request.get(wpURL,function(error,response,body){
-    return body }) })  
-
+    res.send(body) }) })
 
 
 
